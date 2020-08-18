8a0ff4fa59c8f8cced0f72c2612242d812b6989f
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-08-18 03:36:56 +0000

    Appending to the timeline

--- cmd/tl.go
+++ cmd/tl.go
@@ -17,14 +17,25 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
+const (
+	layoutISO  = "2006-01-02"
+	lineFormat = "15:04:05"
+)
+
+type incidentLog *os.File
+
 // tlCmd represents the tl command
 var tlCmd = &cobra.Command{
 	Use:   "tl",
-	Short: "A brief description of your command",
+	Short: "Append a timeline item",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -33,7 +44,38 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("tl called")
+
+		location, err := time.LoadLocation("America/Toronto")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		currentTime := time.Now().In(location)
+		var f *os.File = openOrCreateFile(currentTime)
+		printTimeLine(f, currentTime, strings.Join(args, " "))
+		defer f.Close()
 	},
+}
+
+func printTimeLine(file *os.File, currentTime time.Time, message string) {
+	line := fmt.Sprintf("%s|TIMELINE - %s\n", currentTime.Format(lineFormat), message)
+	if _, err := file.WriteString(line); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openOrCreateFile(date time.Time) *os.File {
+
+	fileName := fmt.Sprintf("%s.md", date.Format(layoutISO))
+	flags := os.O_APPEND | os.O_CREATE | os.O_WRONLY
+	var osPermissions os.FileMode = 0664
+
+	f, err := os.OpenFile(fileName, flags, osPermissions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return f
 }
 
 func init() {
